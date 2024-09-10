26dc8361d9587dabbe8c06ea1c9b9e448ef8a511
choeyuyeon <mmoonn0121@gmail.com>
2024-09-10 19:33:18 +0900

    exercise 2-2 commit

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -12,6 +12,8 @@
 
 package com.nhnacademy.count;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SharedCounter {
@@ -30,31 +32,50 @@ public class SharedCounter {
         /*TODO#1-1 ReentrantLock 생성 합니다.( mutex는 동시에 하나의 Thread만 접근할 수 있습니다. )
            ReentrantLock은 기본적으로 비공정한 락 입니다. 공정성을 보장 하도록 초기화 합니다.
          */
-        ReentrantLock lock = new ReentrantLock(true);
+        mutex = new ReentrantLock(true);
     }
 
     public long getCount(){
         /*TODO#1-2 count 를 반환 합니다. mutex.lock()를 호출하여 다른 thread가 접근할 수 없도록 lock을 걸어 줍니다.
             쓰레드가 작업이 완료되면 mutex.unlock()를 호출하여 잠금을 해제 합니다. 뮤텍스는 lock을 건 쓰레드만 lock을 해제할 수 있습니다.
          */
-        mutex.lock();
-
-        return count;
+        try {
+            mutex.lock();
+            return count;
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }finally {
+            mutex.unlock();
+        }
     }
 
     public long increaseAndGet(){
         /* TODO#1-3 count = count + 1 증가시키고 count를 반환 합니다.
            1-2 처럼 mutex를 이용해서 동기화 될 수 있도록 구현 합니다.
         */
-        count = count + 1;
-        return count;
+        try {
+            mutex.lock();
+            count = count + 1;
+            return count;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            mutex.unlock();
+        }
     }
 
     public long decreaseAndGet(){
         /*TODO#1-4 count = count-1 감소시키고 count를 반환 합니다.
           1-2 처럼 mutex를 이용해서 동기화 될 수 있도록 구현 합니다.
         */
-        count = count - 1;
-        return count;
+        try {
+            mutex.lock();
+            count = count - 1;
+            return count;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            mutex.unlock();
+        }
     }
 }
